2ec704997ca9dbfee08a8224246cc3eb3579a83f
Prince <princerana8@gmail.com>
2025-03-07 12:47:03 -0500

    added a door

---
+++ game_draft/room2_exit.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+var entered = false
+
+#exit between rooms
+
+
+func _on_body_entered(body: Node2D) -> void:
+	entered = true # Replace with function body.
+
+
+func _on_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	entered = false #  Replace with function body.
+
+func _process(delta):
+	if entered == true:
+		if Input.is_action_just_pressed("enter"):
+			get_tree().change_scene_to_file("res://Room3.tscn")
