542f9cbeb57577ab7efffcb80607937228a6dd1b
Kasandra2412 <157088505+Kasandra2412@users.noreply.github.com>
2025-03-13 01:51:43 -0400

    Basic tutorial done

--- game_draft/gui.gd
+++ game_draft/gui.gd
@@ -4,6 +4,7 @@ extends Control
 @export var tutorial : NinePatchRect
 @export var menu : NinePatchRect
 @export var animation_player : AnimationPlayer 
+@onready var quit_icon: Sprite2D = $Tutorial/QuitIcon
 
 
 enum STATE {MENU, TUTORIAL}
@@ -17,7 +18,6 @@ func _input(event):
 				ui_state = STATE.MENU
 				hide_and_show("tutorial", "menu")
 
-	
 	
 	#function to hide and show certain options
 func hide_and_show(first:String, second: String):
@@ -38,3 +38,17 @@ func _on_quit_pressed() -> void:
 #starting the game
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://bacground.tscn") #changing the scene to the opening scene
+
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event.is_action_pressed("click"):
+		match ui_state:
+			STATE.TUTORIAL:
+				ui_state = STATE.MENU
+				hide_and_show("tutorial", "menu")
+
+
+func _on_button_pressed() -> void:
+	ui_state = STATE.MENU
+	hide_and_show("tutorial", "menu") # Replace with function body.
